f7e54e02f6e75b00bb0838646df28ebc5089583f
Loouudee <don.aspecto@gmail.com>
2023-12-02 22:26:11 +0800

    updates

--- src/FileClient.java
+++ src/FileClient.java
@@ -32,14 +32,18 @@ public class FileClient {
 
         switch (action) {
             case "/join":
-                if (socket != null && !socket.isClosed()) {
-                    System.out.println("You are already connected. Please /leave first.");
-                    return;
-                }
+                if(parts.length>=3){
+                    if (socket != null && !socket.isClosed()) {
+                        System.out.println("You are already connected. Please /leave first.");
+                        return;
+                    }
 
-                String serverAddress = parts[1];
-                int port = Integer.parseInt(parts[2]);
-                joinServer(serverAddress, port);
+                    String serverAddress = parts[1];
+                    int port = Integer.parseInt(parts[2]);
+                    joinServer(serverAddress, port);
+                }else{
+                    System.out.println("=== Incomplete parameters ===");
+                }
                 break;
 
             case "/leave":
@@ -47,23 +51,44 @@ public class FileClient {
                 break;
 
             case "/register":
-                String newHandle = parts[1];
-                register(newHandle);
+                if (socket != null && !socket.isClosed()){
+                    if(parts.length >= 2){
+                        String newHandle = parts[1];
+                        register(newHandle);
+                    }else{
+                        System.out.println("=== Please enter a name ===");
+                    }
+                }else{
+                    System.out.println("You are not connected. Please /join first.");
+                }
+                
                 break;
 
             case "/get":
+                if(parts.length>=2){
                 String requestedFile = parts[1];
                 getFile(requestedFile);
+                }else{
+                    System.out.println("=== Please enter a Filename ===");
+                }
                 break;
 
             case "/store":
+                if(parts.length>=2){
                 String fileName = parts[1];
                 storeFile(fileName);
+                }else{
+                    System.out.println("=== Please enter a Filename ===");
+                }
                 break;
 
             case "/dir":
                 listFiles();
                 break;
+            
+            case "/?":
+                help();
+            break;
 
             default:
                 System.out.println("Unknown command: " + command);
@@ -71,13 +96,22 @@ public class FileClient {
         }
     }
 
-    private static void joinServer(String serverAddress, int port) throws IOException {
-        socket = new Socket(serverAddress, port);
-        out = new ObjectOutputStream(socket.getOutputStream());
-        in = new ObjectInputStream(socket.getInputStream());
-
-        System.out.println("Connected to server " + serverAddress + ":" + port);
+    private static void joinServer(String serverAddress, int port) {
+        try {
+            socket = new Socket(serverAddress, port);
+            out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
+    
+            System.out.println("Connected to server " + serverAddress + ":" + port);
+        } catch (UnknownHostException e) {
+            System.err.println("Error: Unknown host " + serverAddress);
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Error: Cannot connect to server " + serverAddress + ":" + port);
+            e.printStackTrace();
+        }
     }
+    
 
     private static void leaveServer() throws IOException {
         if (socket != null && !socket.isClosed()) {
@@ -93,8 +127,12 @@ public class FileClient {
     }
 
     private static void register(String newHandle) throws IOException {
+        if (socket != null && !socket.isClosed()) {
         handle = newHandle;
         System.out.println("Handle set to: " + handle);
+        } else {
+            System.out.println("You are not connected to any server.");
+        }
     }
 
     private static void getFile(String fileName) throws IOException, ClassNotFoundException {
@@ -133,13 +171,40 @@ public class FileClient {
     }
 
     private static void listFiles() throws IOException, ClassNotFoundException {
-        out.writeObject("/dir");
-        List<FileClass> files = (List<FileClass>) in.readObject();
-
-        System.out.println("Files in the server:");
-        for (FileClass file : files) {
-            System.out.println(file.toString());
+        if (socket != null && !socket.isClosed()) {
+            out.writeObject("/dir");
+            Object receivedObject = in.readObject();
+        
+            if (receivedObject instanceof List) {
+                List<?> fileList = (List<?>) receivedObject;
+        
+                System.out.println("Files in the server:");
+                for (Object obj : fileList) {
+                    if (obj instanceof FileClass) {
+                        FileClass file = (FileClass) obj;
+                        System.out.println(file.toString());
+                    }
+                }
+            } else {
+                System.out.println("Server returned an unexpected response.");
+            }
+        } else {
+            System.out.println("You are not connected to any server.");
         }
     }
+
+
+    private static void help(){
+        System.out.println("Available commands:");
+        System.out.println("/join <server_ip_add> <port>    - Connect to the server application");
+        System.out.println("/leave                           - Disconnect from the server application");
+        System.out.println("/register <handle>               - Register a unique handle or alias");
+        System.out.println("/store <filename>                - Send file to server");
+        System.out.println("/dir                             - Request directory file list from a server");
+        System.out.println("/get <filename>                  - Fetch a file from a server");
+        System.out.println("/?                               - Request command help to output all Input Syntax commands for references");
+
+    }
+    
 }
 
