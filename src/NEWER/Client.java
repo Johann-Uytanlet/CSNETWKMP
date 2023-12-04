b02b7e1cef06ef08fea91c4ddf11fdd9799257a7
Johann-Sven-Lee-Uytanlet <114980320+Johann-Sven-Lee-Uytanlet@users.noreply.github.com>
2023-12-04 13:26:54 +0800

    newest changes

--- src/NEWER/Client.java
+++ src/NEWER/Client.java
@@ -2,140 +2,211 @@ package NEWER;
 
 import java.io.*;
 import java.net.*;
-import java.util.*; // For Scanner
-
+import java.util.List;
+import java.util.Scanner;
 
 public class Client {
-    private static final String COMMAND_PREFIX = "/";
+    private static String handle;
+    private static Socket socket;
+    private static ObjectOutputStream out;
+    private static ObjectInputStream in;
+
     public static void main(String[] args) {
-        String host = "localhost"; // can be changed
-        int port = 5000;
+        handle = "Anonymous"; // Default handle
 
-        Scanner sc = new Scanner(System.in);
-        connectToServer(host, port, sc);
+        Scanner scanner = new Scanner(System.in);
 
-        String msg;
-        try {
-            Socket endpoint = new Socket(host, port);
-
-            System.out.println("Client: Has connected to server " + host + ":" + port);
-
-            DataInputStream reader = new DataInputStream(endpoint.getInputStream());
-            DataOutputStream writer = new DataOutputStream(endpoint.getOutputStream());
-
+        while (true) {
             System.out.print("> ");
-            // Let's try inputting a string in the console
-            while (!(msg = sc.nextLine()).equals("END")) {
-                // The message will be send to the server
-                writer.writeUTF(msg);
-                // The Server will append "Server: " so that
-                // we know that the message really was accepted
-                // by the server
-                System.out.println(reader.readUTF());
-                System.out.print("> ");
-            }
+            String command = scanner.nextLine();
 
-            // Send the terminal String to the Server
-            writer.writeUTF("END");
-
-            System.out.println("Client: has terminated connection");
-            sc.close();
-            endpoint.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void connectToServer(String host, int port, Scanner sc) {
-        Socket endpoint;
-        try {
-            endpoint = new Socket(host, port);
-
-            System.out.println("Client: Has connected to server " + host + ":" + port);
-
-            // ... (existing code)
-
-            // Handle user commands
-            handleCommands(endpoint, sc);
-
-            endpoint.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        private static void handleCommands (Socket endpoint, Scanner sc){
             try {
-                DataInputStream reader = new DataInputStream(endpoint.getInputStream());
-                DataOutputStream writer = new DataOutputStream(endpoint.getOutputStream());
-
-                while (true) {
-                    System.out.print("> ");
-                    String input = sc.nextLine();
-
-                    if (input.startsWith(COMMAND_PREFIX)) {
-                        handleCommand(input, writer, reader);
-                    } else {
-                        writer.writeUTF(input); // Send regular message to the server
-                        System.out.println(reader.readUTF());
-                    }
-                }
-            } catch (Exception e) {
+                handleCommand(command);
+            } catch (IOException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
         }
+    }
 
-        private static void handleCommand (String input, DataOutputStream writer, DataInputStream reader) throws
-        IOException {
-            String[] commandParts = input.split(" ");
-            String command = commandParts[0].substring(1); // Remove the leading "/"
+    private static void handleCommand(String command) throws IOException, ClassNotFoundException {
+        String[] parts = command.split(" ");
+        String action = parts[0];
 
-            switch (command) {
-                case "leave":
-                    writer.writeUTF("END");
-                    System.out.println(reader.readUTF());
-                    System.out.println("Client: has terminated connection");
-                    System.exit(0); // Exit the client
-                    break;
-                case "register":
-                    // Handle user registration, if needed
-                    break;
-                case "store":
-                    // Handle file storing
-                    sendFileToServer(commandParts[1], writer, reader);
-                    break;
-                case "dir":
-                    // Request directory file list
-                    writer.writeUTF(command);
-                    System.out.println(reader.readUTF());
-                    break;
-                case "get":
-                    // Fetch a file from the server
-                    writer.writeUTF(command + " " + commandParts[1]);
-                    System.out.println(reader.readUTF());
-                    break;
-                case "?":
-                    // Display command help
-                    displayCommandHelp();
-                    break;
-                default:
-                    System.out.println("Unknown command. Type '/?' for help.");
+        switch (action) {
+            case "/join":
+                if(parts.length>=3){
+                    if (socket != null && !socket.isClosed()) {
+                        System.out.println("You are already connected. Please /leave first.");
+                        return;
+                    }
+
+                    String serverAddress = parts[1];
+                    int port = Integer.parseInt(parts[2]);
+                    joinServer(serverAddress, port);
+                }else{
+                    System.out.println("=== Incomplete parameters ===");
+                }
+                break;
+
+            case "/leave":
+                leaveServer();
+                break;
+
+            case "/register":
+                if (socket != null && !socket.isClosed()){
+                    if(parts.length >= 2){
+                        String newHandle = parts[1];
+                        register(newHandle);
+                    }else{
+                        System.out.println("=== Please enter a name ===");
+                    }
+                }else{
+                    System.out.println("You are not connected. Please /join first.");
+                }
+
+                break;
+
+            case "/get":
+                if(parts.length>=2){
+                    String requestedFile = parts[1];
+                    getFile(requestedFile);
+                }else{
+                    System.out.println("=== Please enter a Filename ===");
+                }
+                break;
+
+            case "/store":
+                if(parts.length>=2){
+                    String fileName = parts[1];
+                    storeFile(fileName);
+                }else{
+                    System.out.println("=== Please enter a Filename ===");
+                }
+                break;
+
+            case "/dir":
+                listFiles();
+                break;
+
+            case "/?":
+                help();
+                break;
+
+            default:
+                System.out.println("Unknown command: " + command);
+                break;
+        }
+    }
+
+    private static void joinServer(String serverAddress, int port) {
+        try {
+            socket = new Socket(serverAddress, port);
+            out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
+
+            System.out.println("Connected to server " + serverAddress + ":" + port);
+        } catch (UnknownHostException e) {
+            System.err.println("Error: Unknown host " + serverAddress);
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Error: Cannot connect to server " + serverAddress + ":" + port);
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void leaveServer() throws IOException {
+        if (socket != null && !socket.isClosed()) {
+            out.writeObject("/disconnect");
+            out.close();
+            in.close();
+            socket.close();
+
+            System.out.println("Disconnected from the server.");
+        } else {
+            System.out.println("You are not connected to any server.");
+        }
+    }
+
+    private static void register(String newHandle) throws IOException {
+        if (socket != null && !socket.isClosed()) {
+            handle = newHandle;
+            System.out.println("Handle set to: " + handle);
+        } else {
+            System.out.println("You are not connected to any server.");
+        }
+    }
+
+    private static void getFile(String fileName) throws IOException, ClassNotFoundException {
+        if (out == null) {
+            System.out.println("You are not connected to any server. Please use /join command.");
+            return;
+        }
+
+        out.writeObject("/get");
+        out.writeObject(fileName);
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            // Receive and write file data to the local file
+            while ((bytesRead = in.read(buffer)) != -1) {
+                fileOutputStream.write(buffer, 0, bytesRead);
             }
-        }
 
-        private static void sendFileToServer (String filename, DataOutputStream writer, DataInputStream reader) throws
-        IOException {
-            // Implement file sending logic here
-            // You can use the FileClass or standard Java File and FileInputStream to send the file
+            System.out.println("File received: " + fileName);
         }
+    }
 
-        private static void displayCommandHelp () {
-            System.out.println("Available commands:");
-            System.out.println("/leave - Disconnect from the server");
-            System.out.println("/register <handle> - Register a unique handle or alias");
-            System.out.println("/store <filename> - Send file to server");
-            System.out.println("/dir - Request directory file list from the server");
-            System.out.println("/get <filename> - Fetch a file from the server");
-            System.out.println("/? - Request command help");
+
+    private static void storeFile(String fileName) throws IOException {
+        if (out == null) {
+            System.out.println("You are not connected to any server. Please use /join command.");
+            return;
         }
+        File fileToStore = new File(fileName);
+        FileClass file = new FileClass(fileToStore, handle);
+        out.writeObject("/store");
+        out.writeObject(file);
+        System.out.println("File stored: " + file);
+    }
+
+    private static void listFiles() throws IOException, ClassNotFoundException {
+        if (socket != null && !socket.isClosed()) {
+            out.writeObject("/dir");
+            Object receivedObject = in.readObject();
+
+            if (receivedObject instanceof List) {
+                List<?> fileList = (List<?>) receivedObject;
+
+                System.out.println("Files in the server:");
+                for (Object obj : fileList) {
+                    if (obj instanceof FileClass) {
+                        FileClass file = (FileClass) obj;
+                        System.out.println(file.toString());
+                    }
+                }
+            } else {
+                System.out.println("Server returned an unexpected response.");
+            }
+        } else {
+            System.out.println("You are not connected to any server.");
+        }
+    }
+
+
+    private static void help(){
+        System.out.println("Available commands:");
+        System.out.println("/join <server_ip_add> <port>    - Connect to the server application");
+        System.out.println("/leave                           - Disconnect from the server application");
+        System.out.println("/register <handle>               - Register a unique handle or alias");
+        System.out.println("/store <filename>                - Send file to server");
+        System.out.println("/dir                             - Request directory file list from a server");
+        System.out.println("/get <filename>                  - Fetch a file from a server");
+        System.out.println("/?                               - Request command help to output all Input Syntax commands for references");
 
     }
+
+}
+
--- src/NEWER/Server.java
+++ src/NEWER/Server.java
@@ -1,31 +1,93 @@
 package NEWER;
 
+import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
+    private static List<FileClass> files = new ArrayList<>();
 
     public static void main(String[] args) {
-        int port = 5000; // Can be changed
+        int port = 12345; // Change this to the desired port
 
         try {
-            ServerSocket ss = new ServerSocket(port);
-
-            System.out.println("Sever: Listening to port " + port);
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("File Server is running on port " + port);
 
             while (true) {
-                // Waits for a client to connect
-                Socket endpoint = ss.accept();
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected: " + clientSocket);
 
-                System.out.println("Server: Client at " + endpoint.getRemoteSocketAddress() + " has connected");
-
-                // Make the Thread Object
-                Connection connect = new Connection(endpoint);
-                // Start the thread
-                connect.start();
+                // Handle client in a new thread
+                new Thread(() -> handleClient(clientSocket)).start();
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private static void handleClient(Socket clientSocket) {
+        try (
+                ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+                ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream())
+        ) {
+            while (true) {
+                String command = (String) in.readObject();
+
+                switch (command) {
+                    case "/store":
+                        FileClass file = (FileClass) in.readObject();
+                        System.out.println(file);
+                        files.add(file);
+                        System.out.println("File stored: " + file);
+                        break;
+
+                    case "/dir":
+                        out.writeObject(files);
+                        break;
+
+                    case "/get":
+                        String fileName = (String) in.readObject();
+                        sendFile(out, fileName);
+                        break;
+
+                    case "/disconnect":
+                        System.out.println("Client disconnected: " + clientSocket);
+                        return;
+
+                    default:
+                        System.out.println("Unknown command: " + command);
+                        break;
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void sendFile(ObjectOutputStream out, String fileName) throws IOException {
+        for (FileClass file : files) {
+            if (file.getFile().getName().equals(fileName)) {
+                try (FileInputStream fileInputStream = new FileInputStream(file.getFile())) {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+
+                    // Send the file data in chunks
+                    while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                        out.write(buffer, 0, bytesRead);
+                    }
+
+                    // Signal the end of file transfer
+                    out.writeObject(null);
+
+                    System.out.println("File sent: " + file);
+                    return;
+                }
+            }
+        }
+
+        // If the file is not found, inform the client
+        out.writeObject(null);
+    }
 }
